01d5569668f53c9cc852658649c38de5c7249688
Dele <oladele@gmail.com>
2013-07-04 13:26:32 -0400

    cleared duplicated migrations remove_index and add_user_id from commit

--- db/migrate/20130704133228_remove_index_barcode_custom_in_items.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIndexBarcodeCustomInItems < ActiveRecord::Migration
-  def up
-  	remove_index :items, :barcode_custom
-  end
-
-  def down
-  	add_index :items, :barcode_custom, unique: true
-  end
-end
